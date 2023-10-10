4947b6a54d0626b738f59854a8163f0f3976f078
adri <agarcia20211@iesjoanramis.org>
2023-10-10 19:28:58 +0200

    canvis fets

--- programa1.java
+++ programa1.java
@@ -1,5 +1,27 @@
-//hola soc n'adria
-//comentarios de de la rama modification
-//venim a modificar desde es master
-//Otro comentario desde la rama modification
-//Modificacio desde comentaris
+package edact4;
+
+public class edact {
+
+		    public static void main(String[] args) {
+		       Scanner scanner = new Scanner(System.in);
+		        
+		        System.out.print("Introdueix el primer nombre: ");
+		        double nombre1 = scanner.nextDouble();
+		        
+		        System.out.print("Introdueix el segon nombre: ");
+		        double nombre2 = scanner.nextDouble();
+		        
+		        double producte = nombre1 * nombre2;
+		        
+		        
+		        System.out.println("El producte de " + nombre1 + " i " + nombre2 + " és " + producte);
+		        
+		       
+		        scanner.close();
+		    }
+		}
+
+
+
+
+
